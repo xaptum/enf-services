0dd4c10a076020965391522001433e6f26be95b9
Zane Beckwith <zanebeckwith@gmail.com>
2021-02-02 16:20:42 -0500

    enftun: Allow enftun conf file to be overridden.

--- enftun/entrypoint.sh
+++ enftun/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "Setting up enftun"
-/usr/bin/enftun-setup up /etc/enftun/enf0.conf
-/usr/bin/enftun -c /etc/enftun/enf0.conf &
+if [[ -f "/data/enf0/enf0.conf" ]]; then
+    CONF_FILE="/data/enf0/enf0.conf"
+else
+    CONF_FILE="/etc/enftun/enf0.conf.default"
+fi
+
+echo "Setting up enftun using config file '${CONF_FILE}'"
+/usr/bin/enftun-setup up $CONF_FILE 
+/usr/bin/enftun -c $CONF_FILE &
 
 exec "$@"
